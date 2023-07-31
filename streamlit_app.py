4db4121c4a086dd4a174b802d8bcc316c21f6ae5
dschiff01 <58717231+heredre@users.noreply.github.com>
2023-07-31 15:38:18 -0400

    Update streamlit_app.py
    updates with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,12 @@ import streamlit
 
 streamlit.title('My Parents new healthy dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled free-range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled free-range egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
